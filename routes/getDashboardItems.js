af8fe75b5720a0fcc954dd9a1b059c7c71e0c487
m-salamon <Moishe@Salamons-MBP.home>
2018-09-01 23:55:43 -0400

    minor changes

--- routes/getDashboardItems.js
+++ routes/getDashboardItems.js
@@ -3,14 +3,14 @@ var router = express.Router();
 var repo = require('../repo');
 
 router.get('/', (req, res) => {
-    let projectItems = repo.getDashboardItems.getDashboardItems(req.query)
+  repo.getDashboardItems.getDashboardItems(req.query)
     .then((dashboardItems) => {
       console.log('RESPONSE ', dashboardItems)
-        res.json({ success: true, dashboardItems: dashboardItems });
+      res.json({ success: true, dashboardItems: dashboardItems });
     }).catch(err => {
-        console.error(err)
+      console.error(err)
     })
-    
+
 });
 
 module.exports = router;
