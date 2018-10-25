7b0e2002fbf035fa55984b793efae32f6c58b3bf
m-salamon <Moishe@Salamons-MacBook-Pro.local>
2018-10-24 21:04:36 -0400

    database changes

--- migrations/20170828142507_add_teams_table.js
+++ migrations/20170828142507_add_teams_table.js
@@ -4,6 +4,7 @@ exports.up = function (knex, Promise) {
     table.bigIncrements('id').primary();
     table.string('name').notNullable()
     table.bigInteger('adminId').notNullable()
+    table.bigInteger('isPublic').notNullable()
     table.timestamp('createdAt').default(knex.fn.now())
     table.timestamp('updatedAt').defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')) 
   });
@@ -12,3 +13,4 @@ exports.up = function (knex, Promise) {
 exports.down = function (knex, Promise) {
   return knex.schema.dropTable('teams');
 };
+           
--- migrations/20170828142539_add_users_table.js
+++ migrations/20170828142539_add_users_table.js
@@ -10,7 +10,7 @@ exports.up = function(knex, Promise) {
        table.string('tempToken').notNullable()
        table.boolean('isVerified').notNullable()
        table.string('hourlyRate').notNullable()
-       table.bigInteger('teamId').notNullable()
+      //  table.bigInteger('teamId').notNullable()
        table.string('ip').notNullable()
        table.boolean('blackListed').notNullable()
        table.timestamp('createdAt').default(knex.fn.now())
